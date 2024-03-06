6d695bff388e2c0e4b5ad665ab2877af57a26a09
pslaulhe <pslaulhe@gmail.com>
2024-03-06 15:08:17 +0100

    Add sinon for stubs; Write diagnostic control unit test skeleton

--- TypeScript/telemetry-system-tests/telemetry-diagnostic-controls-test.ts
+++ TypeScript/telemetry-system-tests/telemetry-diagnostic-controls-test.ts
@@ -1,24 +1,28 @@
 import { expect } from 'chai';
-import * as jest from 'jest';
 import 'mocha';
+import * as sinon from "sinon";
+import TelemetryClient from "../telemetry-system/telemetry-client";
 import TelemetryDiagnosticControls from '../telemetry-system/telemetry-diagnostic-controls';
 
 describe('Telemetry System', () => {
 
 	describe('TelemetryDiagnosticControls', () => {
-
+		const telemtryClient = new TelemetryClient();
 		it('CheckTransmission should send a diagnostic message and receive a status message response', () => {
-			const telemetryDiagnosticControls = new TelemetryDiagnosticControls();
+			const telemetryDiagnosticControls = new TelemetryDiagnosticControls(telemtryClient);
 			telemetryDiagnosticControls.checkTransmission();
 			const diagnosticInfo = telemetryDiagnosticControls.readDiagnosticInfo();
 			// tslint:disable-next-line:no-unused-expression
 			expect(diagnosticInfo).not.empty;
 		});
 
-		it('Connect telemetry client',() => {
-			jest.
+		it('Connect telemetry client happy path',() => {
+			sinon.stub(telemtryClient, 'connect');
 		});
 
+		it('Test telemetry client connection offline ', () => {});
+
+		it('Test telemetry client connection offline until 3rd try', () => {});
 	});
 
 });
